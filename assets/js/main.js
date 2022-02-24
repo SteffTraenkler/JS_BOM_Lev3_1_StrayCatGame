60507d4f94dc76c4fd678eb36395187f55a61bf0
SuperCoder <supercoder@MacBook-Air-von-SuperCoder-2.local>
2022-02-24 20:51:40 +0100

    comm

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+let catImg = document.getElementById('cat')
+console.log(catImg);
+
+let cat
+let catPos = 0;
+var speed = 300;
+
+function turn() {
+    if (catImg.style.transform == 'scaleX(1)') {
+        catImg.style.transform = 'scaleX(-1)'
+
+
+    } else if (catImg.style.transform == 'scaleX(-1)') {
+        catImg.style.transform = 'scaleX(1)'
+    }
+}
+
+function catWalk() {
+    clearInterval(cat);
+    cat = setInterval(function walk() {
+        if (catImg.style.transform == 'scaleX(1)') {
+            catPos += 20
+            console.log('cat walks to the left')
+            catImg.style.left = `${catPos}px`
+        } else if (catImg.style.transform == 'scaleX(-1)') {
+            catPos -= 20
+            console.log('cat walks to the right')
+            catImg.style.left = `${catPos}px`
+        }
+    }, 300);
+}
+
+function catSpeed() {
+    clearInterval(cat);
+    cat = setInterval(function walk() {
+        if (catImg.style.transform == 'scaleX(1)') {
+            catPos += 20
+            console.log('cat walks to the left')
+            catImg.style.left = `${catPos}px`
+        } else if (catImg.style.transform == 'scaleX(-1)') {
+            catPos -= 20
+            console.log('cat walks to the right')
+            catImg.style.left = `${catPos}px`
+        }
+    }, 50)
+}
+
+// function pause() {
+//     clearInterval(cat);
+// }
+
+// document.body.addEventListener(keypress)
